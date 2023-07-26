3f53703fc946bfc9d1959aaf4eacc2470ec042c2
caseystell <cleigh14@hotmail.com>
2023-07-26 08:35:58 -0600

    new view features

---
+++ public/javascripts/main.js
@@ -0,0 +1,11 @@
+// const unfulfilledCheckBox = document.getElementById("unfulfilled-btn");
+// const careReqLogEl = document.getElementById("care-request-log");
+
+// unfulfilledCheckBox.addEventListener("click", hideFulfilled);
+
+// function hideFulfilled() {
+    
+//     if (unfulfilledCheckBox.checked && ) {
+
+//     }
+// }
