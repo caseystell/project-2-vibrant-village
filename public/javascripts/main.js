0853a5b6802f51119a6eae15d8e7d2c2b874a2f1
caseystell <cleigh14@hotmail.com>
2023-08-31 15:28:00 -0600

    updating views

---
+++ public/javascripts/main.js
@@ -0,0 +1,14 @@
+const tableDataEl = document.getElementsByClassName("tbl-data");
+
+// function hideRow() {
+//     let tr = table.rows;
+//     for (let i = 0; i < tr.length; i++) {
+//         let td = tr[i].cells;
+//         console.log(`This is td: ${td}`)
+//     //     let td_fulfilled = td[4].innerText;
+//     //     if (td_fulfilled.indexOf(tableDataEl))
+//     }
+// }
+
+// hideRow()
+console.log(tableDataEl)
